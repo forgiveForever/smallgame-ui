fc6be0c3d26c785353e18ec76de1c9165d84f2d4
Liqiang <1396961884@qq.com>
2019-06-19 18:48:51 +0800

    增加用户信息

---
+++ public/tool.js
@@ -0,0 +1,21 @@
+function loadJs(url,callback){
+    var script=document.createElement('script');
+    script.type="text/javascript";
+    if(typeof(callback)!="undefined"){
+    if(script.readyState){
+    script.onreadystatechange=function(){
+     if(script.readyState == "loaded" || script.readyState == "complete"){
+     script.onreadystatechange=null;
+     callback();
+     }
+    }
+    }else{
+    script.onload=function(){
+     callback();
+    }
+    }
+    }
+    script.src=url;
+    //document.body.appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
+    }
--- public/websocketTool.js
+++ public/websocketTool.js
@@ -1,29 +1,32 @@
 
 // type 0 代表第一次传送用户标识，
-    var ws = new WebSocket('ws://47.107.105.43:8001');
-	var ws = new WebSocket('ws://47.107.105.43:8001');
+   // var ws = new WebSocket('ws://47.107.105.43:8001');
+	var ws ;
     var session = window.sessionStorage;
-    ws.onopen = function(e){
-                console.log("连接服务器成功");
-                var userInfo = JSON.parse(session.getItem('userInfo'));
-                let info={
-                    userInfo:userInfo,
-                    sendtype:0,
-                    returntype:0,
-                    returnroom:null,
-                    returnisclear:false,
-                    draw_info:null,
-                    draw_uid:null,
-                }
-                ws.send(JSON.stringify(info));
+    function initWS(){
+        ws.onopen = function(e){
+            console.log("连接服务器成功");
+            var userInfo = JSON.parse(session.getItem('userInfo'));
+            let info={
+                userInfo:userInfo,
+                sendtype:0,
+                returntype:0,
+                returnroom:null,
+                returnisclear:false,
+                draw_info:null,
+                draw_uid:null,
+            }
+            ws.send(JSON.stringify(info));
 
-            }
-            ws.onclose = function(e){
-                console.log("服务器关闭");
-            }
-            ws.onerror = function(){
-                console.log("连接出错");
-            }
+        }
+        ws.onclose = function(e){
+            console.log("服务器关闭");
+        }
+        ws.onerror = function(){
+            console.log("连接出错");
+        }
+    }
+   
 
             // ws.onmessage = function(e){
             //     mess.innerHTML = "连接成功"
@@ -34,5 +37,7 @@
             // }
 
         function getWebsocket(){
+           ws= new WebSocket('ws://47.107.105.43:8001');
+           initWS();
             return ws;
         }
