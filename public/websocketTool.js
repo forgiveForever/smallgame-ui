0796ac8a1c74623aa6ba34bb9c63a8508765a200
Liqiang <1396961884@qq.com>
2019-06-21 15:51:35 +0800

    添加用户头像用户断开等操作

--- public/websocketTool.js
+++ public/websocketTool.js
@@ -1,11 +1,16 @@
 
 // type 0 代表第一次传送用户标识，
-    var ws = new WebSocket('ws://47.107.105.43:8001');
-	//var ws = new WebSocket('ws://127.0.0.1:8001');
+   // const ip ='ws://47.107.105.43:8001';
+    const ip ='ws://127.0.0.1:8001';
+	var ws ;
     var session = window.sessionStorage;
-    ws.onopen = function(e){
-                console.log("连接服务器成功");
-                var userInfo = JSON.parse(session.getItem('userInfo'));
+
+    function initWebSocket(){
+        ws.onopen = function(e){
+            console.log("连接服务器成功");
+            
+            var userInfo = JSON.parse(session.getItem('userInfo'));
+           
                 let info={
                     userInfo:userInfo,
                     sendtype:0,
@@ -16,14 +21,18 @@
                     draw_uid:null,
                 }
                 ws.send(JSON.stringify(info));
-
-            }
-            ws.onclose = function(e){
-                console.log("服务器关闭");
-            }
-            ws.onerror = function(){
-                console.log("连接出错");
-            }
+    
+           
+          
+        }
+        ws.onclose = function(e){
+            console.log("服务器关闭");
+        }
+        ws.onerror = function(){
+            console.log("连接出错");
+        }
+    }
+   
 
             // ws.onmessage = function(e){
             //     mess.innerHTML = "连接成功"
@@ -34,5 +43,10 @@
             // }
 
         function getWebsocket(){
+            if(userInfo){
+                ws = new WebSocket(ip);
+                initWebSocket();
+            }
+           
             return ws;
         }
